deca928ca372b57df2f636b4189294e6e5bc3904
Cheolwon Jang <39438165+losskatsu@users.noreply.github.com>
2019-12-09 22:11:52 +0900

    Create OddOccurrencesInArray_02.py

---
+++ OddOccurrencesInArray_02.py
@@ -0,0 +1,24 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.6
+    uniqA = list(set(A))
+    dicA = {}
+    
+    for i in range(0, len(uniqA)):
+        dicA[uniqA[i]] = 0
+    
+    for ele in A:
+        for key in dicA.keys():
+            if(ele==key):
+                dicA[key] += 1
+    
+    for key in dicA.keys():
+        if(dicA[key]%2 == 1):
+            res = key
+            break
+
+    return res
+    
+    
