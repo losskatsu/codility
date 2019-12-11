c477a19ad5d59508599678d118218060f36f8b53
Cheolwon Jang <39438165+losskatsu@users.noreply.github.com>
2019-12-11 21:24:59 +0900

    Update PassingCars_01.py

--- PassingCars_01.py
+++ PassingCars_01.py
@@ -1,6 +1,9 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
+# 50 %
+# 시간복잡도 O(N ** 2)
+
 def solution(A):
     # write your code in Python 3.6
     res = 0
