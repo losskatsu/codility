a82413180ebb51f3b22f657b73c8a401c2ed8390
Cheolwon Jang <39438165+losskatsu@users.noreply.github.com>
2019-12-11 21:23:36 +0900

    Create PassingCars_01.py

---
+++ PassingCars_01.py
@@ -0,0 +1,18 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.6
+    res = 0
+    N = len(A)
+    
+    for i in range(0, N):
+        if(res>1000000000):
+            return -1
+        if(A[i]==1):
+            continue
+        for j in range(i+1, N):
+            if(A[i]!=A[j]):
+                res += 1
+    
+    return res
